1fa2043340abb6dd6c8332975c68b81d0f519a01
Mpendulo Sibiya <mpendulo@thedigitalacademy.co.za>
2021-07-10 22:58:35 +0200

    country servcie in main

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class CountriesService {
 
-  constructor() { }
+  countryAPI = 'https://restcountries.eu/rest/v2/all';
+  constructor(private http: HttpClient) { }
+
+  // countries API method
+  getCountries() {
+    return this.http.get(this.countryAPI);
+  }
 }
