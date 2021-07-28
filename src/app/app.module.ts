8d1dafde7243245a3c1ed2951e16ad3bf7f5eef4
mpendulo-dev <mpendulo.dev@gmail.com>
2021-07-28 10:02:58 +0200

    removed unsued components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { StatsCardsComponent } from './components/stats-cards/stats-cards.component';
 
 // pagination module
 import {NgxPaginationModule} from 'ngx-pagination';
-import { FooterComponent } from './components/footer/footer.component';
+
+// Piper
 import { FormsModule } from '@angular/forms';
 import { CountryFilterPipe } from './components/main-content/country-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     MainContentComponent,
     StatsCardsComponent,
-    FooterComponent,
     CountryFilterPipe
   ],
   imports: [
