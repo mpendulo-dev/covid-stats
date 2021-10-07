f28e686004a61ae2e8868cbdce30e0efc41b3c0d
Mpendulo Sibiya <mpendulodev@gmail.com>
2021-10-07 15:44:43 +0200

    modified API service

--- src/app/models/country.model.ts
+++ src/app/models/country.model.ts
@@ -1,7 +1,12 @@
-export class Country { 
+export class Country {
 
     country: string;
     continent: string;
     population: number;
-    
+    countryInfo: any;
+    active: any;
+    cases: any;
+    recovered: any;
+    deaths: any;
+
 }
--- src/app/services/covid-api.service.ts
+++ src/app/services/covid-api.service.ts
@@ -1,5 +1,7 @@
+import { Country } from './../models/country.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,7 +16,7 @@ export class CovidApiService {
     constructor(private http: HttpClient) { }
 
    // Method that return the covid api data
-    getData() {
-      return this.http.get(this.covidUrl);
+    getData(): Observable<Country[]> {
+      return this.http.get<Country[]>(this.covidUrl);
     }
 }
